6f2a582584056a43455d3a8ed8a29c1c39bca445
Pouyan <pouyan.zarbafian@hotmail.com>
2021-10-21 09:25:56 -0400

    connect server to db

--- server/app.js
+++ server/app.js
@@ -2,11 +2,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { Pool } = require('pg');
+const connectionString = 'postgres://bdewvnazghaqsm:929f434a9513430580257674b3162c0efa5e10a143eb9a64fa97baf3885e3e95@ec2-50-19-210-145.compute-1.amazonaws.com:5432/d9j35ma9cv42t5';
+const pool = new Pool({connectionString, ssl: {
+  rejectUnauthorized: false},});
 
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
