51815d3c3b37535afb460901269f348f13579d66
Pouyan <pouyan.zarbafian@hotmail.com>
2021-10-21 12:08:41 -0400

    add driver's name to orders list

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
-    
-    db.query('SELECT * FROM orders')
+
+    db.query('SELECT orders.id, description, cost, revenue, drivers.name as drive_name FROM orders JOIN drivers ON drivers.id = driver_id')
       .then(data => res.json(data.rows))
       .catch(err => res.send(err));
   });
+
+
+
 
   return router;
 };
