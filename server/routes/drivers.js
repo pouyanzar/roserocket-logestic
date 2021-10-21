2ccfd9611542772bdb9a35c74279975ec00c9064
Pouyan <pouyan.zarbafian@hotmail.com>
2021-10-21 13:12:22 -0400

    retrieve information for whole list or a specific id

--- server/routes/drivers.js
+++ server/routes/drivers.js
@@ -5,11 +5,18 @@ const router = express.Router();
 module.exports = (db) => {
 
   router.get('/', function(req, res) {
-    db.query('SELECT * FROM drivers')
+    db.query('SELECT * FROM drivers JOIN orders ON drivers.id = driver_id')
       .then(data => res.send(data.rows))
       .catch(err => res.send(err));
   });
 
+  router.get('/:driverId', (req, res) => {
+    const driverId = req.params.driverId;
+    db.query(`SELECT * FROM drivers JOIN orders ON drivers.id = driver_id WHERE drivers.id = $1`, [driverId])
+      .then(data => res.send(data.rows))
+      .catch(err => res.send(err));
+  });
+  
   return router;
 };
 
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -2,15 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
 
-    db.query('SELECT orders.id, description, cost, revenue, drivers.name as drive_name FROM orders JOIN drivers ON drivers.id = driver_id')
+  router.get('/', (req, res) => {
+    db.query('SELECT * FROM orders JOIN drivers ON drivers.id = driver_id')
       .then(data => res.json(data.rows))
       .catch(err => res.send(err));
   });
 
+  router.get('/:orderId', (req, res) => {
+    const orderId = req.params.orderId;
+    db.query(`SELECT * FROM orders JOIN drivers ON drivers.id = driver_id WHERE orders.id = $1`, [orderId])
+      .then(data => res.send(data.rows))
+      .catch(err => res.send(err));
+  });
 
-
-
+  
   return router;
 };
