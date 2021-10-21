9ec048a72cbc2d2dc84975a1a867e9335900f44b
Pouyan <pouyan.zarbafian@hotmail.com>
2021-10-21 13:19:23 -0400

    get the list of unassigned orders

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -8,7 +8,13 @@ module.exports = (db) => {
       .then(data => res.json(data.rows))
       .catch(err => res.send(err));
   });
-
+  
+  router.get('/unassigned', (req, res) => {
+    db.query(`SELECT * FROM orders WHERE driver_id IS NULL`)
+      .then(data => res.send(data.rows))
+      .catch(err => res.send(err));
+  });
+  
   router.get('/:orderId', (req, res) => {
     const orderId = req.params.orderId;
     db.query(`SELECT * FROM orders JOIN drivers ON drivers.id = driver_id WHERE orders.id = $1`, [orderId])
@@ -16,6 +22,5 @@ module.exports = (db) => {
       .catch(err => res.send(err));
   });
 
-  
   return router;
 };
